1b15d93400a917b6b6fe1efe9af655f82fa0e3fa
Mike Barberry <mikebarberry@protonmail.com>
2020-09-18 04:04:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,11 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [5, 6, 7],
+    [0, 4, 7],
+    [2, 4, 5]
+]
